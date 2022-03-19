ab9fe1486111f6d2084db460c1f39e776e714ec0
siryu-saito <saito.s.ryu@gmail.com>
2022-03-19 13:46:50 +0900

    バリデーションチェック 文法エラー訂正

--- app/models/problem.rb
+++ app/models/problem.rb
@@ -4,7 +4,7 @@ class Problem < ApplicationRecord
   has_many :answers, dependent: :destroy
   belongs_to :user
 
-  validates :title, presence: true, {length: {maximum: 60} }
+  validates :title, presence: true, length: {maximum: 60}
   validates :statement, presence: true, {length: {maximum: 1000} }
   validates :answer, presence: true, {length: {maximum: 30} }
   validates :incorrect1, presence: true, {length: {maximum: 30} }
