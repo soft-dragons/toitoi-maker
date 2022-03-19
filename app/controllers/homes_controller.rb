c1d0e7f1ad48be3fdab8d4a16216c87b183a5c92
sakamoto <gyakusankaku739@gmail.com>
2022-03-20 05:07:38 +0900

    一応

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,6 +1,16 @@
 class HomesController < ApplicationController
 
   def top
+    if user_signed_in?
+       @users_array = User.pluck(:name, :point).sort_by(&:last).reverse
+       num = 0
+       @users_array.each do |user|
+         if current_user.name == user[0]
+            @rank = num + 1
+         end
+         num = num + 1
+       end
+    end
   end
 
 end
