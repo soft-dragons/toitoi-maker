2bb596e642f8522324a76c761ae38533678780b0
kmtszky <79302477+kmtszky@users.noreply.github.com>
2022-03-19 18:34:05 +0900

    [add] answers_controller

--- config/routes.rb
+++ config/routes.rb
@@ -6,9 +6,11 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'homes#top'
 
-  resources :problems
   get 'myProblem' => 'problems#myProblem',   as: 'myProblem'
   get 'test' => 'problems#test',             as: 'test'
   get 'test_index' => 'problems#test_index', as: 'test_index'
   get 'toitoi' => 'problems#toitoi',         as: 'toitoi'
+  resources :problems do
+    resources :answers, only: [:create]
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_03_19_075759) do
+ActiveRecord::Schema.define(version: 2022_03_19_092620) do
 
   create_table "answers", force: :cascade do |t|
     t.integer "user_id", null: false
@@ -18,7 +18,7 @@ ActiveRecord::Schema.define(version: 2022_03_19_075759) do
     t.boolean "result", default: false, null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["user_id", "problem_id"], name: "index_answers_on_user_id_and_problem_id", unique: true
+    t.index ["problem_id"], name: "index_answers_on_problem_id"
     t.index ["user_id"], name: "index_answers_on_user_id"
   end
 
