2c1cfa02568ce992775034fd9084b4371d0c04d9
kmtszky <79302477+kmtszky@users.noreply.github.com>
2022-03-19 17:22:37 +0900

    [fix] seed実装

---
+++ db/migrate/20220319075759_change_index.rb
@@ -0,0 +1,8 @@
+class ChangeIndex < ActiveRecord::Migration[5.2]
+  def change
+    remove_index :answers,   :problem_id
+    remove_index :feedbacks, :problem_id
+    add_index :answers,   [:user_id, :problem_id], unique: true
+    add_index :feedbacks, :problem_id
+  end
+end
